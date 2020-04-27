ea6a93ed8521277ce0d7e9467195947ea468e160
kamil-gorny <kamil.gorny99@gmail.com>
2020-04-27 02:53:23 +0200

    some initial comments in javascript file

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+//Task Controller
+var taskController = (function(){
+    //Add Item 
+    //Delete Item
+})();
+//Sounds Controller
+var soundsController = (function(){
+    //Start sound
+    //Set Volume
+})();
+
+//Pomodoro Controller
+var pomodoroController = (function(){
+
+})();
+
+//UI Controller
+var uiControoler = (function(){
+
+})();
+
+//Global App Controller
+var globalController = (function(soundsCtrl, uiCtrl, globalCtrl, taskCtrl){
+
+})(soundsController, uiControoler, globalController, taskController);
