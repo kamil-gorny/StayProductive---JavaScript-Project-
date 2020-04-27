7cd8fe4832bb9e778401cfa8212a44f2afb670a9
kamil-gorny <kamil.gorny99@gmail.com>
2020-04-27 22:58:14 +0200

    added noisly feature that allows to start and stop certain sounds

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-
-
 //Task Controller
 var taskController = (function(){
+    var TaskBoard = function(id, name){
+        this.id;
+        this.name;
+    }
+
+    var Task = function(id, description){
+        this.id = id;
+        this.description = description;
+    }
     //Add Item 
     //Delete Item
 })();
+
 //Sounds Controller
 var soundsController = (function(){
+    return {
+        toggleSound: function(sound){
+            var element = document.getElementById(sound+"-btn");
+            var audio = document.querySelector("#"+sound);
+            if(element.classList.contains(sound+"active")){
+                audio.play();
+            }
+            else{
+                audio.pause();
+            }
+                
+        }
+        
+    }
     //Start sound
     //Set Volume
 })();
+   
+
 
 //Pomodoro Controller
 var pomodoroController = (function(){
@@ -17,11 +41,48 @@ var pomodoroController = (function(){
 })();
 
 //UI Controller
-var uiControoler = (function(){
+var uiController = (function(){
+    var DOMStrings = {
+      buttons: '.buttons',
+    };
 
+    return{
+        getDomStrings: function(){
+            return DOMStrings;
+        }
+    }
 })();
 
 //Global App Controller
 var globalController = (function(soundsCtrl, uiCtrl, globalCtrl, taskCtrl){
+    var DOM = uiCtrl.getDomStrings();
+   
+    var setupEventListeners = function(){
+        document.querySelector(DOM.buttons).addEventListener('click', playSoundController);
+    };
+    
+    var playSoundController = function(event){
+        var soundBtnID = event.target.id;
 
-})(soundsController, uiControoler, globalController, taskController);
+        if(soundBtnID){
+            var splitID = soundBtnID.split("-");
+            var soundName = splitID[0];
+
+            document.getElementById(soundBtnID).classList.toggle(soundName+"active");
+            document.getElementById(soundBtnID).classList.toggle("iconnotactive");
+            
+            soundsCtrl.toggleSound(soundName);
+        }
+        console.log(soundBtnID);
+        console.log(soundName);
+    };
+
+    return{
+        init: function(){
+            setupEventListeners();
+        }
+    }
+
+})(soundsController, uiController, globalController, taskController);
+
+globalController.init();
